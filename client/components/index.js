d9c1786cfdf6bee25f81f837a875a75645b52ea4
dotsalot <lara.seren13@gmail.com>
2019-08-15 17:30:09 -0400

    removed unnecessary files

--- client/components/index.js
+++ client/components/index.js
@@ -4,9 +4,6 @@
  * having to figure out which file they belong to!
  */
 export {default as Navbar} from './navbar'
-export {default as UserHome} from './user-home'
-export {default as Chatroom} from './chat'
-export {Login, Signup} from './auth-form'
 export {default as NewSignup} from './Signup'
 export {default as NewLogin} from './Login'
 export {default as NewChat} from './Chatroom'
--- client/routes.js
+++ client/routes.js
@@ -1,42 +1,19 @@
 import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {withRouter, Route, Switch} from 'react-router-dom'
-import PropTypes from 'prop-types'
-import {
-  UserHome,
-  Chatroom,
-  NewSignup,
-  NewLogin,
-  NewChat,
-  Gamepage
-} from './components'
-import {me} from './store'
+import {Route, Switch} from 'react-router-dom'
+import {Chatroom, NewSignup, NewLogin, NewChat, Gamepage} from './components'
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
-  componentDidMount() {
-    this.props.loadInitialData()
-  }
-
   render() {
-    const {isLoggedIn} = this.props
-
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={NewLogin} />
         <Route path="/signup" component={NewSignup} />
-        {isLoggedIn && (
-          <Switch>
-            {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={UserHome} />
-          </Switch>
-        )}
         {/* Displays our Login component as a fallback */}
-        <Route path="/chat" component={Chatroom} />
-        <Route path="/newchat" component={NewChat} />
+        <Route path="/chat" component={NewChat} />
         <Route path="/game" component={Gamepage} />
         <Route component={NewLogin} />
       </Switch>
@@ -44,33 +21,4 @@ class Routes extends Component {
   }
 }
 
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
-    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id
-  }
-}
-
-const mapDispatch = dispatch => {
-  return {
-    loadInitialData() {
-      // dispatch(me())
-    }
-  }
-}
-
-// The `withRouter` wrapper makes sure that updates are not blocked
-// when the url changes
-export default withRouter(connect(mapState, mapDispatch)(Routes))
-
-/**
- * PROP TYPES
- */
-Routes.propTypes = {
-  loadInitialData: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
+export default Routes
