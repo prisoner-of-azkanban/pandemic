15158bff8481eb95d4067ec4c669414d9b663222
Rebecca Rossman <rrossman25@gmail.com>
2019-08-20 15:43:03 -0400

    cubes, pawns placed

--- client/components/PandemicMap.js
+++ client/components/PandemicMap.js
@@ -11,23 +11,98 @@ class PandemicMap extends React.Component {
     this.drawRect()
   }
   drawRect = () => {
-    console.log('i was clicked')
     const canvas = this.canvasRef.current
     const ctx = canvas.getContext('2d')
-    const image = this.refs.image
     ctx.font = '10px Courier New'
+    // eslint-disable-next-line max-statements
     Object.keys(this.cityList).forEach(cityKey => {
       const city = this.cityList[cityKey]
+
+      // City
       ctx.beginPath()
       ctx.fillStyle = 'rgba(255, 255, 255, 1)'
       ctx.arc(city.coords[0], city.coords[1], 10, 0, 2 * Math.PI)
       ctx.fillStyle = city.color
       ctx.fill()
-      ctx.stroke()
       ctx.fillStyle = 'black'
-      ctx.fillText(city.name, city.coords[0] - 20, city.coords[1] - 15)
+      ctx.fillText(city.name, city.coords[0] - 20, city.coords[1] + 20)
+
+      // Research Center
+      ctx.beginPath()
+      ctx.arc(city.coords[0], city.coords[1], 3, 0, 2 * Math.PI)
+      ctx.fillStyle = 'white'
+      ctx.fill()
+      ctx.stroke()
+
+      // Disease Cubes
+      ctx.beginPath()
+      ctx.rect(city.coords[0] - 20, city.coords[1] - 23, 10, 10)
+      ctx.fillStyle = 'blue'
+      ctx.fill()
+
+      ctx.beginPath()
+      ctx.rect(city.coords[0] - 10, city.coords[1] - 23, 10, 10)
+      ctx.fillStyle = 'yellow'
+      ctx.fill()
+
+      ctx.beginPath()
+      ctx.rect(city.coords[0], city.coords[1] - 23, 10, 10)
+      ctx.fillStyle = 'black'
+      ctx.fill()
+
+      ctx.beginPath()
+      ctx.rect(city.coords[0] + 10, city.coords[1] - 23, 10, 10)
+      ctx.fillStyle = 'red'
+      ctx.fill()
+
+      // Disease Cube Numbers
+      ctx.beginPath()
+      ctx.fillStyle = 'white'
+      ctx.fillText(city.blue, city.coords[0] - 18, city.coords[1] - 15)
+
+      ctx.beginPath()
+      ctx.fillStyle = 'black'
+      ctx.fillText(city.yellow, city.coords[0] - 8, city.coords[1] - 15)
+
+      ctx.beginPath()
+      ctx.fillStyle = 'white'
+      ctx.fillText(city.black, city.coords[0] + 3, city.coords[1] - 15)
+
+      ctx.beginPath()
+      ctx.fillStyle = 'black'
+      ctx.fillText(city.red, city.coords[0] + 13, city.coords[1] - 15)
+
+      // Player Pawns
+      ctx.beginPath()
+      ctx.arc(city.coords[0] - 6, city.coords[1] - 6, 3, 0, 2 * Math.PI)
+      ctx.fillStyle = 'lime'
+      ctx.fill()
+      ctx.stroke()
+
+      ctx.beginPath()
+      ctx.arc(city.coords[0] + 6, city.coords[1] + 6, 3, 0, 2 * Math.PI)
+      ctx.fillStyle = 'pink'
+      ctx.fill()
+      ctx.stroke()
+
+      ctx.beginPath()
+      ctx.arc(city.coords[0] + 6, city.coords[1] - 6, 3, 0, 2 * Math.PI)
+      ctx.fillStyle = 'aqua'
+      ctx.fill()
+      ctx.stroke()
+
+      ctx.beginPath()
+      ctx.arc(city.coords[0] - 6, city.coords[1] + 6, 3, 0, 2 * Math.PI)
+      ctx.fillStyle = 'brown'
+      ctx.fill()
+      ctx.stroke()
     })
   }
+
+  // drawPawn = (city) => {
+
+  // }
+
   render() {
     return (
       <React.Fragment>
