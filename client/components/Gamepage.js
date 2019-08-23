b5ebc1bb46f748f12a16df17b0b510c707964e07
Yuting Zhang <yutingzhang729@gmail.com>
2019-08-23 12:02:39 -0400

    changing username get

--- client/components/Gamepage.js
+++ client/components/Gamepage.js
@@ -44,15 +44,15 @@ class Gamepage extends React.Component {
   }
 
   render() {
-    return this.state.username ? (
+    return this.props.username ? (
       <div id="gamepage">
         <GamePlay
           gamename={this.props.match.params.gamename}
-          username={this.state.username}
+          username={this.props.username}
         />
         <Chatroom
           gamename={this.props.match.params.gamename}
-          username={this.state.username}
+          username={this.props.username}
           className="chat-container"
         />
 
--- client/routes.js
+++ client/routes.js
@@ -11,11 +11,45 @@ import {
   Lose,
   Win
 } from './components'
+import firebase from 'firebase'
+import {app, db, config} from '../firebase-server/firebase'
 
 /**
  * COMPONENT
  */
 class Routes extends Component {
+  constructor() {
+    super()
+    this.state = {
+      username: ''
+    }
+    this._isMounted = false
+  }
+
+  async componentDidMount() {
+    this._isMounted = true
+    let userId = ''
+    let username = 'Guest'
+    await firebase.auth().onAuthStateChanged(loggedinUser => {
+      if (loggedinUser) {
+        userId = loggedinUser.uid
+        db
+          .collection('users')
+          .doc(userId)
+          .get()
+          .then(doc => {
+            if (doc.exists) {
+              username = doc.data().username
+            }
+          })
+          .then(() => {
+            if (this._isMounted) {
+              this.setState({username: username})
+            }
+          })
+      }
+    })
+  }
   render() {
     return (
       <Switch>
@@ -24,7 +58,9 @@ class Routes extends Component {
         <Route
           exact
           path="/game/:gamename"
-          render={props => <Gamepage {...props} />}
+          render={props => (
+            <Gamepage {...props} username={this.state.username} />
+          )}
         />
         <Route path="/chat" component={NewChat} />
         <Route path="/game" component={Gamepage} />
