ad7a072ef3a37b513d73fa5637a117115d6d510b
Rebecca Rossman <rrossman25@gmail.com>
2019-08-26 16:56:32 -0400

    travis fix

--- firebase-server/firebase.js
+++ firebase-server/firebase.js
@@ -1,18 +1,11 @@
 const firebase = require('firebase')
 require('firebase/firestore')
+const {config} = require('./secrets')
 
-let config = {
-  apiKey: process.env.GFB_KEY,
-  authDomain: 'pandemic-online-38cdd.firebaseapp.com',
-  databaseURL: 'https://pandemic-online-38cdd.firebaseio.com',
-  projectId: 'pandemic-online-38cdd',
-  storageBucket: '',
-  messagingSenderId: '517649764640',
-  appId: '1:517649764640:web:043e7a9de43c0663'
-}
+console.log(config)
 
 let app = firebase.initializeApp(config)
 
 const db = firebase.firestore()
 
-module.exports = {db, app, config}
+module.exports = {db, app}
